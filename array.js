fde5a333877d5c395ed1f628d08e323228b740cb
durgarao <asapusirisha@gmail.com>
2020-03-06 17:12:09 +0530

    array example :external js file

---
+++ array.js
@@ -0,0 +1,15 @@
+var x=[5,3,83];/*By array literal*/
+console.log(x[2]);
+var exam=["ms","ds","ml","capp"];
+console.log(exam[1]);
+var lan=["html","css","javascript","angular","react"];
+console.log(lan);
+var arr=new Array();/*using new kwyword */
+arr[0]="sirisha";
+arr[1]="shivangi";
+arr[3]="mohsin";
+arr[4]="justin bieber";
+console.log(arr);
+var yrkkh=new Array("mohsin khan","shivangi joshi","tanmay rishi");/**array constuctor */
+console.log(yrkkh);
+console.log(yrkkh[1]);
