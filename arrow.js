d302e5c770c58beb7ce8228bace181ad41dd1222
durgarao <asapusirisha@gmail.com>
2020-03-10 10:42:12 +0530

    arrow function

---
+++ arrow.js
@@ -0,0 +1,8 @@
+
+var fruit;
+fruit = () =>
+    {
+var arr=["orange","mango","pinapple","kiwi","banana","apple","apricot","cherry","blueberry","raspberry","dates","mulberry","papaya","strawberry","watermelon","jackfruit","grapes"];
+var x=arr.sort(); 
+document.getElementById("fru").innerHTML=x.join("<br>");
+}
